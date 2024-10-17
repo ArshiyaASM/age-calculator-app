f31e89197977605bc7c9335ea70678e606086e68
ArshiyaASM <arshiyaasemaniw@gmail.com>
2024-10-17 19:16:30 +0330

    fix the day age bug

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const day = document.querySelector(".day_age");
 const label = document.querySelectorAll("label");
 
 
-let checker = true;
-let checker1 = true;
-let checker2 = true;
-let checker3 = true;
+let checker = false;
+let checker1 = false;
+let checker2 = false;
+let checker3 = false;
+
+
+console.log(input)
 
 
 // for days input
@@ -76,7 +79,7 @@ input[0].addEventListener("blur" , ()=>{
 input[1].addEventListener("blur" , ()=>{
 
 
-    new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject)=>{
         if(!(input[1].value.length == 0) || !(input[1].value == " ")){
             resolve()
             checker1 = true;
@@ -88,7 +91,7 @@ input[1].addEventListener("blur" , ()=>{
             reject() 
         }
     }).then(()=>{
-        new Promise((reslove, reject)=>{
+        return new Promise((reslove, reject)=>{
             if(Number(input[1].value) <= 12 && Number(input[1].value) > 0){
                 reslove();
                 checker1 = true;
@@ -101,7 +104,7 @@ input[1].addEventListener("blur" , ()=>{
             }
         })
     }).then(()=>{
-        new Promise((reslove, reject)=>{
+        return new Promise((reslove, reject)=>{
             for(let item of input[1].value){
                 if(Number(item) || item == 0){
                     reslove();
@@ -225,37 +228,8 @@ button.addEventListener("click" , ()=>{
         }else {
             reject();
         }
-
     }).then(()=>{
-        checker2 = true;
-        input[2].style.border = "1px solid #6f6b6b";
-        input[2].style.color = "#6f6b6b";
-        error[2].innerHTML = "";
-        label[2].style.color = "#6f6b6b";
-
-        checker1 = true;
-        input[1].style.border = "1px solid #6f6b6b";
-        input[1].style.color = "#6f6b6b";
-        error[1].innerHTML = "";
-        label[1].style.color = "#6f6b6b";
-
-        checker = true;
-        input[0].style.border = "1px solid #6f6b6b";
-        input[0].style.color = "#6f6b6b";
-        error[0].innerHTML = "";
-        label[0].style.color = "#6f6b6b";
-
-        new Promise((resolve, reject)=>{
-            const toDayDate = new moment();
-            const dateInInput = new moment(input[2].value+"-"+input[1].value+"-"+input[0].value);
-            const different = toDayDate.diff(dateInInput);
-            if(different > 0) {
-                resolve()
-            }else {
-                reject()
-            }
-    
-        }).then(()=>{
+        return new Promise((resolve, reject)=>{
             checker2 = true;
             input[2].style.border = "1px solid #6f6b6b";
             input[2].style.color = "#6f6b6b";
@@ -274,67 +248,87 @@ button.addEventListener("click" , ()=>{
             error[0].innerHTML = "";
             label[0].style.color = "#6f6b6b";
 
-            checker3 = true;
 
-        }).catch(()=>{
-            checker2 = false;
-            input[2].style.border = "1px solid rgb(255, 87, 87)";
-            input[2].style.color = "rgb(255, 87, 87)";
-            error[2].innerHTML = "Must be valid year";
-            label[2].style.color = "rgb(255, 87, 87)";
+            let different = new Date() - new Date(input[2].value+"-"+input[1].value+"-"+input[0].value);
+            console.log(different)
+            if(different > 0) {
+                resolve()
+            }
+            
+            else {
+                reject()
+            }
 
-            checker1 = false;
-            input[1].style.border = "1px solid rgb(255, 87, 87)";
-            input[1].style.color = "rgb(255, 87, 87)";
-            error[1].innerHTML = "Must be valid month";
-            label[1].style.color = "rgb(255, 87, 87)";
-
-            checker = false;
-            input[0].style.border = "1px solid rgb(255, 87, 87)";
-            input[0].style.color = "rgb(255, 87, 87)";
-            error[0].innerHTML = "Must be valid day";
-            label[0].style.color = "rgb(255, 87, 87)";
-            checker3 = false ;
         })
-
+    }).then(()=>{
+        checker2 = true;
+        input[2].style.border = "1px solid #6f6b6b";
+        input[2].style.color = "#6f6b6b";
+        error[2].innerHTML = "";
+        label[2].style.color = "#6f6b6b";
+    
+        checker1 = true;
+        input[1].style.border = "1px solid #6f6b6b";
+        input[1].style.color = "#6f6b6b";
+        error[1].innerHTML = "";
+        label[1].style.color = "#6f6b6b";
+    
+        checker = true;
+        input[0].style.border = "1px solid #6f6b6b";
+        input[0].style.color = "#6f6b6b";
+        error[0].innerHTML = "";
+        label[0].style.color = "#6f6b6b";
         
 
-        
-        if(checker3 == true){
+        if(input[0].value.length == 1){
+            input[0].value = `0${input[0].value}`;
+            console.log(input[0].value);
+        }
 
-            const dob = new Date((input[2].value+"-"+input[1].value+"-"+input[0].value));
-            const currentDate = new Date();
-
-            const ageInMilliseconds = currentDate - dob;
-            const ageInYears = ageInMilliseconds / (365 * 24 * 60 * 60 * 1000);
-            const age = Math.floor(ageInYears);
-
-            currentDate.setFullYear(currentDate.getFullYear() - age);
-            const monthDiff = currentDate.getMonth() - dob.getMonth();
-
-            let months = monthDiff;
-            if (currentDate.getDate() < dob.getDate()) {
-                months--;
-            }
-
-            if(currentDate.getMonth() < dob.getMonth()){
-                months += 12;
-            }
-
-            let days = currentDate.getDate() - dob.getDate();
-            if (days < 0) {
-                const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-                days = lastDayOfMonth - dob.getDate() + currentDate.getDate();
-            }
-
-            year.innerHTML = age;
-            day.innerHTML =days;
-            month.innerHTML = months;
+        if(input[1].value.length == 1){
+            input[1].value = `0${input[1].value}`;
+            console.log(input[1].value);
         }
 
 
 
-    }).catch(()=>{
+        let dob = new Date((input[2].value+"-"+input[1].value+"-"+input[0].value + "T00:00:00"));
+        console.log(dob);
+
+        const currentDate = new Date();
+        const utcOffset = currentDate.getTimezoneOffset() * 60000;
+        const UTCcurrentDate = new Date(currentDate.getTime() + utcOffset);
+
+
+        let age = UTCcurrentDate.getFullYear() - dob.getFullYear();
+        year.innerHTML = age;
+
+        let monthDiff = UTCcurrentDate.getMonth() - dob.getMonth();
+        let dayDiff = UTCcurrentDate.getDate() - dob.getDate();
+
+        if(monthDiff < 0 || monthDiff === 0 && dayDiff < 0){
+            age--;
+        }
+
+
+
+
+        if(dayDiff < 0){
+            const dayInPrevMonth = new Date(UTCcurrentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+            dayDiff = dayInPrevMonth + UTCcurrentDate.getDate() - dob.getDate();
+            monthDiff--;
+        }
+
+        if(monthDiff < 0){
+            monthDiff += 12
+        }
+
+        year.innerHTML = age;
+        month.innerHTML = monthDiff;
+        day.innerHTML = dayDiff;
+
+    }).catch((Error)=>{
+        console.log(Error)
         checker2 = false;
         input[2].style.border = "1px solid rgb(255, 87, 87)";
         input[2].style.color = "rgb(255, 87, 87)";
@@ -352,5 +346,9 @@ button.addEventListener("click" , ()=>{
         input[0].style.color = "rgb(255, 87, 87)";
         error[0].innerHTML = "Must be valid day";
         label[0].style.color = "rgb(255, 87, 87)";
+        checker3 = false;
     })
 })
+
+
+
